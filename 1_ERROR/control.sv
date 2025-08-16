ec96af148814c39519f7b0e36cfa4215520f1745
alien flip <alienflipsf@gmail.com>
2025-08-16 22:28:22 +0100

    further efficiency upgrades

--- 1_ERROR/control.sv
+++ 1_ERROR/control.sv
@@ -2,36 +2,40 @@ module control
 (
     input               button_0,
     input               button_1,
-    input               clk,                    // Clock input signal
-    output  logic [2:0] led                     // 3-bit LED output
+    input               clk,
+    output  logic [2:0] led
 );
+    parameter RED=3'b110, BLUE=3'b011, GREEN=3'b101;
+
     parameter STOP=0, START=1;
-    logic state;
+    
+    logic [2:0] state;
 
-    logic [31:0] counter;                       // 32-bit counter register
+    logic [31:0] counter;
 
-    initial begin
-        state   <= STOP;
-        counter <= 31'd0;                       // Initialize counter to 0
-        led     <= 3'b110;                      // Initialize LED pattern
-    end
-
-    always @(posedge clk) begin
+    always @ (posedge clk) begin
         if (button_0 == 0) state <= STOP;
 
         if (button_1 == 0) begin
             state   <= START;
-            counter <= 31'd1350_0000;
+            counter <= 31'd6000_0000;
         end
 
-        if (state == START) begin
-            if (counter < 31'd1350_0000)
-                counter <= counter + 1;         // Increment counter
-            else begin
-                counter <= 31'd0;               // Reset counter
-                led[2:0] <= {led[1:0],led[2]};  // Rotate LED pattern left
-            end
+        else if (state == START) begin
+            if (counter < 31'd6000_0000)
+                counter <= counter + 1;
+            else
+                counter <= 31'd0;
         end
+    end
+
+    always @ (posedge clk) begin
+        case (counter)
+            31'd0000_0000 : led <= RED;
+            31'd3000_0000 : led <= BLUE;
+            31'd6000_0000 : led <= GREEN;
+            default:begin end
+        endcase
     end
 
 endmodule
